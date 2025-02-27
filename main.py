b4dce9d1363dd31a485a8b6e096a13f1afcdc16f
marekmchl <marekmchl@proton.me>
2025-02-27 18:35:47 +0100

    Window opens and closes

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        # Makes the windows close button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(0)
+        # -- end of loop --
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
