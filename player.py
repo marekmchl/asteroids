3c8e2e3bb0c7c2b7cb891b0a10fb7e2dbb8f4759
marekmchl <marekmchl@proton.me>
2025-02-28 08:59:25 +0100

    Add firerate/cooldown mechanic

--- player.py
+++ player.py
@@ -3,13 +3,14 @@ from circleshape import CircleShape
 from shot import Shot
 from constants import (
     PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED,
-    PLAYER_SHOOT_SPEED
+    PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 )
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -38,10 +39,17 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+        if self.timer > 0:
+            self.timer -= dt
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.timer > 0:
+            return
+        else:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.timer += PLAYER_SHOOT_COOLDOWN
